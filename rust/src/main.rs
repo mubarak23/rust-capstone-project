08a690ede406df0ef605cf01ec19d1802ca41739
mubarak23 <mubarakaminu340@gmail.com>
2025-07-25 21:12:16 +0100

    chore: fix rpc

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -14,8 +14,8 @@ use std::io::Write;
 
 // Node access params
 const RPC_URL: &str = "http://127.0.0.1:18443"; // Default regtest RPC port
-const RPC_USER: &str = "mubarak23";
-const RPC_PASS: &str = "mubarak23";
+const RPC_USER: &str = "alice";
+const RPC_PASS: &str = "password";
 
 // You can use calls not provided in RPC lib API using the generic `call` function.
 // An example of using the `send` RPC call, which doesn't have exposed API.
